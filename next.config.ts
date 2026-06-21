c31c00ac8e9f02a8569db9ea6378b75a628de5ef
Ali Seyedi <aliseyedi07@gmail.com>
2026-06-21 16:31:55 +0330

    fix: add static export configuration for Netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export", // این خط باعث می‌شود خروجی استاتیک تولید شود
+    images: {
+        unoptimized: true, // برای نمایش تصاویر در خروجی استاتیک
+    },
+    trailingSlash: true, // برای سازگاری با نتلیف
 };
 
 export default nextConfig;
